cfd265fef62462528b3dcaa6f12b6fb9ac178091
faizan619 <alamf6023@gmail.com>
2024-05-12 22:02:21 +0530

    adding font from google fonts|

---
+++ src/app/style/fonts.js
@@ -0,0 +1,338 @@
+import {
+  AR_One_Sans,
+  Advent_Pro,
+  Afacad,
+  Akshar,
+  Albert_Sans,
+  Alegreya,
+  Aleo,
+  Alexandria,
+  Alumni_Sans,
+  Andada_Pro,
+  Anek_Bangla,
+  Anek_Gujarati,
+  Anek_Devanagari,
+  Anek_Gurmukhi,
+  Anek_Latin,
+  Anek_Kannada,
+  Anek_Malayalam,
+  Anek_Odia,
+  Anek_Tamil,
+  Anek_Telugu,
+  Antonio,
+  Anuphan,
+  Anybody,
+  Archivo,
+  Archivo_Narrow,
+  Arima,
+  Arimo,
+  Asap,
+  Assistant,
+  Azeret_Mono,
+  Ballet,
+  Baloo_2,
+  Baloo_Bhai_2,
+  Baloo_Bhaijaan_2,
+  Baloo_Bhaina_2,
+  Baloo_Chettan_2,
+  Baloo_Da_2,
+  Baloo_Paaji_2,
+  Baloo_Tamma_2,
+  Baloo_Tammudu_2,
+  Baloo_Thambi_2,
+  Besley,
+  Big_Shoulders_Display,
+  Big_Shoulders_Inline_Display,
+  Big_Shoulders_Inline_Text,
+  Big_Shoulders_Text,
+  Bitter,
+  Bodoni_Moda,
+  Brygada_1918,
+  Cabin,
+  Cairo,
+  Cairo_Play,
+  Catamaran,
+  Caveat,
+  Changa,
+  Chivo,
+  Chivo_Mono,
+  Cinzel,
+  Climate_Crisis,
+  Comme,
+  Commissioner,
+  Cormorant,
+  Crimson_Pro,
+  Cuprum,
+  DM_Sans,
+  Dancing_Script,
+  Darker_Grotesque,
+  Domine,
+  Dosis,
+  EB_Garamond,
+  Eczar,
+  Edu_NSW_ACT_Foundation,
+  Edu_QLD_Beginner,
+  Edu_SA_Beginner,
+  Edu_TAS_Beginner,
+  Edu_VIC_WA_NT_Beginner,
+  Encode_Sans,
+  Encode_Sans_SC,
+  Epilogue,
+  Exo,
+  Exo_2,
+  Expletus_Sans,
+  Familjen_Grotesk,
+  Faustina,
+  Figtree,
+  Finlandica,
+  Fira_Code,
+  Foldit,
+  Frank_Ruhl_Libre,
+  Fraunces,
+  Fredoka,
+  Gabarito,
+  Gantari,
+  Gelasio,
+  Gemunu_Libre,
+  Genos,
+  Geologica,
+  Georama,
+  Glory,
+  Gluten,
+  Golos_Text,
+  Grandstander,
+  Grenze_Gotisch,
+  Hahmlet,
+  Handjet,
+  Hanken_Grotesk,
+  Hedvig_Letters_Serif,
+  Heebo,
+  Hepta_Slab,
+  Honk,
+} from "next/font/google";
+
+const ar_one_sans = AR_One_Sans({ preload: false });
+const advent_pro = Advent_Pro({ preload: false });
+const afacad = Afacad({ preload: false });
+const akshar = Akshar({ preload: false });
+const albert_sans = Albert_Sans({ preload: false });
+const algereya = Alegreya({ preload: false });
+const aleo = Aleo({ preload: false });
+const alexandria = Alexandria({ preload: false });
+const alumini_sans = Alumni_Sans({ preload: false });
+const andada_pro = Andada_Pro({ preload: false });
+const anek_bangla = Anek_Bangla({ preload: false });
+const anek_gujarati = Anek_Gujarati({ preload: false });
+const anek_devanagari = Anek_Devanagari({ preload: false });
+const anek_gurmukhi = Anek_Gurmukhi({ preload: false });
+const anek_latin = Anek_Latin({ preload: false });
+const anek_kannada = Anek_Kannada({ preload: false });
+const anek_malayalam = Anek_Malayalam({ preload: false });
+const anek_odia = Anek_Odia({ preload: false });
+const anek_tamil = Anek_Tamil({ preload: false });
+const anek_telugu = Anek_Telugu({ preload: false });
+const antonio = Antonio({ preload: false });
+const anuphan = Anuphan({ preload: false });
+const anybody = Anybody({ preload: false });
+const archivo = Archivo({ preload: false });
+const archivo_narrow = Archivo_Narrow({ preload: false });
+const arima = Arima({ preload: false });
+const arimo = Arimo({ preload: false });
+const asap = Asap({ preload: false });
+const assistant = Assistant({ preload: false });
+const azeret_mono = Azeret_Mono({ preload: false });
+const ballet = Ballet({ preload: false });
+const baloo_2 = Baloo_2({ preload: false });
+const baloo_bhai_2 = Baloo_Bhai_2({ preload: false });
+const baloo_bhaijaan_2 = Baloo_Bhaijaan_2({ preload: false });
+const baloo_bhaina_2 = Baloo_Bhaina_2({ preload: false });
+const baloo_chettan_2 = Baloo_Chettan_2({ preload: false });
+const baloo_da_2 = Baloo_Da_2({ preload: false });
+const baloo_paaji_2 = Baloo_Paaji_2({ preload: false });
+const baloo_tamma_2 = Baloo_Tamma_2({ preload: false });
+const baloo_tammudu_2 = Baloo_Tammudu_2({ preload: false });
+const baloo_thambi_2 = Baloo_Thambi_2({ preload: false });
+const besley = Besley({ preload: false });
+const big_shoulders_display = Big_Shoulders_Display({ preload: false });
+const big_shoulders_inline_display = Big_Shoulders_Inline_Display({ preload: false });
+const big_shoulders_inline_text = Big_Shoulders_Inline_Text({ preload: false });
+const big_shoulders_text = Big_Shoulders_Text({ preload: false });
+// const biorhyme = BioRhyme({ preload: false });
+const bitter = Bitter({ preload: false });
+const bodoni_moda = Bodoni_Moda({ preload: false });
+const brygada_1918 = Brygada_1918({ preload: false });
+const cabin = Cabin({ preload: false });
+const cairo = Cairo({ preload: false });
+const cairo_play = Cairo_Play({ preload: false });
+const catamaran = Catamaran({ preload: false });
+const caveat = Caveat({ preload: false });
+const changa = Changa({ preload: false });
+const chivo = Chivo({ preload: false });
+const chivo_mono = Chivo_Mono({ preload: false });
+const cinzel = Cinzel({ preload: false });
+const climate_crisis = Climate_Crisis({ preload: false });
+const comme = Comme({ preload: false });
+const commissioner = Commissioner({ preload: false });
+const cormorant = Cormorant({ preload: false });
+const crimson_pro = Crimson_Pro({ preload: false });
+const cuprum = Cuprum({ preload: false });
+const dm_sans = DM_Sans({ preload: false });
+const dancing_script = Dancing_Script({ preload: false });
+const darker_grotesque = Darker_Grotesque({ preload: false });
+const domine = Domine({ preload: false });
+const dosis = Dosis({ preload: false });
+const eb_garamond = EB_Garamond({ preload: false });
+const eczar = Eczar({ preload: false });
+const edu_nsw_act_foundation = Edu_NSW_ACT_Foundation({ preload: false });
+const edu_qld_beginner = Edu_QLD_Beginner({ preload: false });
+const edu_sa_beginner = Edu_SA_Beginner({ preload: false });
+const edu_tas_beginner = Edu_TAS_Beginner({ preload: false });
+const edu_vic_wa_nt_beginner = Edu_VIC_WA_NT_Beginner({ preload: false });
+const encode_sans = Encode_Sans({ preload: false });
+const encode_sans_sc = Encode_Sans_SC({ preload: false });
+const epilogue = Epilogue({ preload: false });
+const exo = Exo({ preload: false });
+const exo_2 = Exo_2({ preload: false });
+const expletus_sans = Expletus_Sans({ preload: false });
+const familjen_grotesk = Familjen_Grotesk({ preload: false });
+const faustina = Faustina({ preload: false });
+const figtree = Figtree({ preload: false });
+const finlandica = Finlandica({ preload: false });
+const fira_code = Fira_Code({ preload: false });
+const foldit = Foldit({ preload: false });
+const frank_ruhl_libre = Frank_Ruhl_Libre({ preload: false });
+const fraunces = Fraunces({ preload: false });
+const fredoka = Fredoka({ preload: false });
+const gabarito = Gabarito({ preload: false });
+const gantari = Gantari({ preload: false });
+const gelasio = Gelasio({ preload: false });
+const gemunu_libre = Gemunu_Libre({ preload: false });
+const genos = Genos({ preload: false });
+const geologica = Geologica({ preload: false });
+const georama = Georama({ preload: false });
+const glory = Glory({ preload: false });
+const gluten = Gluten({ preload: false });
+const golos_text = Golos_Text({ preload: false });
+const grandstander = Grandstander({ preload: false });
+const grenze_gotisch = Grenze_Gotisch({ preload: false });
+const hahmlet = Hahmlet({ preload: false });
+const handjet = Handjet({ preload: false });
+const hanken_grotesk = Hanken_Grotesk({ preload: false });
+const hedvig_letters_serif = Hedvig_Letters_Serif({ preload: false });
+const heebo = Heebo({ preload: false });
+const hepta_slab = Hepta_Slab({ preload: false });
+const honk = Honk({ preload: false });
+
+
+export {
+  ar_one_sans,
+  advent_pro,
+  afacad,
+  akshar,
+  albert_sans,
+  algereya,
+  aleo,
+  alexandria,
+  alumini_sans,
+  andada_pro,
+  anek_bangla,
+  anek_gujarati,
+  anek_devanagari,
+  anek_gurmukhi,
+  anek_latin,
+  anek_kannada,
+  anek_malayalam,
+  anek_odia,
+  anek_tamil,
+  anek_telugu,
+  antonio,
+  anuphan,
+  anybody,
+  archivo,
+  archivo_narrow,
+  arima,
+  arimo,
+  asap,
+  assistant,
+  azeret_mono,
+  ballet,
+  baloo_2,
+  baloo_bhai_2,
+  baloo_bhaijaan_2,
+  baloo_bhaina_2,
+  baloo_chettan_2,
+  baloo_da_2,
+  baloo_paaji_2,
+  baloo_tamma_2,
+  baloo_tammudu_2,
+  baloo_thambi_2,
+  besley,
+  big_shoulders_display,
+  big_shoulders_inline_display,
+  big_shoulders_inline_text,
+  big_shoulders_text,
+  bitter,
+  bodoni_moda,
+  brygada_1918,
+  cabin,
+  cairo,
+  cairo_play,
+  catamaran,
+  caveat,
+  changa,
+  chivo,
+  chivo_mono,
+  cinzel,
+  climate_crisis,
+  comme,
+  commissioner,
+  cormorant,
+  crimson_pro,
+  cuprum,
+  dm_sans,
+  dancing_script,
+  darker_grotesque,
+  domine,
+  dosis,
+  eb_garamond,
+  eczar,
+  edu_nsw_act_foundation,
+  edu_qld_beginner,
+  edu_sa_beginner,
+  edu_tas_beginner,
+  edu_vic_wa_nt_beginner,
+  encode_sans,
+  encode_sans_sc,
+  epilogue,
+  exo,
+  exo_2,
+  expletus_sans,
+  familjen_grotesk,
+  faustina,
+  figtree,
+  finlandica,
+  fira_code,
+  foldit,
+  frank_ruhl_libre,
+  fraunces,
+  fredoka,
+  gabarito,
+  gantari,
+  gelasio,
+  gemunu_libre,
+  genos,
+  geologica,
+  georama,
+  glory,
+  gluten,
+  golos_text,
+  grandstander,
+  grenze_gotisch,
+  hahmlet,
+  handjet,
+  hanken_grotesk,
+  hedvig_letters_serif,
+  heebo,
+  hepta_slab,
+  honk
+};
