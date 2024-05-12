1a81efe965edd58f626a5f4a6d9610cd720e44dc
faizan619 <alamf6023@gmail.com>
2024-05-13 01:00:29 +0530

    finally done with font importing

--- src/app/style/fonts.js
+++ src/app/style/fonts.js
@@ -109,8 +109,170 @@ import {
   Heebo,
   Hepta_Slab,
   Honk,
-} from "next/font/google";
+  Ibarra_Real_Nova,
+  Imbue,
+  Inconsolata,
+  Instrument_Sans,
+  Inter,
+  Inter_Tight,
+  JetBrains_Mono,
+  Josefin_Sans,
+  Josefin_Slab,
+  Jost,
+  Jura,
+  Kablammo,
+  Kalnia,
+  Kameron,
+  Karla,
+  Kode_Mono,
+  Kreon,
+  Kufam,
+  Kumbh_Sans,
+  Labrada,
+  League_Gothic,
+  Lemonada,
+  Lexend,
+  Lexend_Deca,
+  Lexend_Giga,
+  Lexend_Mega,
+  Lexend_Peta,
+  Lexend_Tera,
+  Lexend_Zetta,
+  Libre_Bodoni,
+  Libre_Franklin,
+  Linefont,
+  Literata,
+  Lora,
+  M_PLUS_1,
+  M_PLUS_1_Code,
+  M_PLUS_2,
+  M_PLUS_Code_Latin,
+  Mada,
+  Manrope,
+  Marhey,
+  Markazi_Text,
+  Martian_Mono,
+  Maven_Pro,
+  Meera_Inimai,
+  Merriweather_Sans,
+  Mohave,
+  Montagu_Slab,
+  Montserrat,
+  Mulish,
+  MuseoModerno,
+  Nabla,
+  Noto_Emoji,
+  Noto_Kufi_Arabic,
+  Noto_Naskh_Arabic,
+  Noto_Nastaliq_Urdu,
+  Noto_Rashi_Hebrew,
+  Noto_Sans,
+  Noto_Sans_Adlam,
+  Noto_Sans_Adlam_Unjoined,
+  Noto_Sans_Arabic,
+  Nunito,
+  Nunito_Sans,
+  Ojuju,
+  Onest,
+  Open_Sans,
+  Orbitron,
+  Oswald,
+  Outfit,
+  Overpass,
+  Overpass_Mono,
+  Oxanium,
+  Pathway_Extreme,
+  Petrona,
+  Phudu,
+  Piazzolla,
+  Pixelify_Sans,
+  Platypi,
+  Playfair,
+  Playfair_Display,
+  Playpen_Sans,
+  Plus_Jakarta_Sans,
+  Podkova,
+  Poltawski_Nowy,
+  Pontano_Sans,
+  Public_Sans,
+  Quicksand,
+  REM,
+  Radio_Canada,
+  Raleway,
+  Rasa,
+  Readex_Pro,
+  Recursive,
+  Red_Hat_Display,
+  Red_Hat_Mono,
+  Red_Hat_Text,
+  Red_Rose,
+  Reddit_Mono,
+  Reem_Kufi,
+  Reem_Kufi_Fun,
+  Rethink_Sans,
+  Roboto_Condensed,
+  Roboto_Flex,
+  Roboto_Mono,
+  Roboto_Serif,
+  Roboto_Slab,
+  Rosario,
+  Rubik,
+  Ruda,
+  STIX_Two_Text,
+  Saira,
+  Sansita_Swashed,
+  Schibsted_Grotesk,
+  Sen,
+  Shantell_Sans,
+  Signika,
+  Sixtyfour,
+  Smooch_Sans,
+  Sofia_Sans,
+  Sofia_Sans_Condensed,
+  Sofia_Sans_Extra_Condensed,
+  Sofia_Sans_Semi_Condensed,
+  Sometype_Mono,
+  Sono,
+  Sora,
+  Source_Code_Pro,
+  Source_Sans_3,
+  Source_Serif_4,
+  Space_Grotesk,
+  Spline_Sans,
+  Spline_Sans_Mono,
+  Syne,
+  Teko,
+  Tektur,
+  Texturina,
+  Tilt_Neon,
+  Tilt_Prism,
+  Tourney,
+  Trispace,
+  Truculenta,
+  Ubuntu,
+  Ubuntu_Mono,
+  Unbounded,
+  Urbanist,
+  Varta,
+  Vazirmatn,
+  Victor_Mono,
+  Vollkorn,
+  Wavefont,
+  Wix_Madefor_Display,
+  Wix_Madefor_Text,
+  Work_Sans,
+  Workbench,
+  Yaldevi,
+  Yanone_Kaffeesatz,
+  Yrsa,
+  Ysabeau,
+  Ysabeau_Infant,
+  Ysabeau_Office,
+  Ysabeau_SC,
 
+
+} from "next/font/google";
+// abcdefghijklmnopqrstu vwxyz
 const ar_one_sans = AR_One_Sans({ preload: false });
 const advent_pro = Advent_Pro({ preload: false });
 const afacad = Afacad({ preload: false });
@@ -222,7 +384,166 @@ const hedvig_letters_serif = Hedvig_Letters_Serif({ preload: false });
 const heebo = Heebo({ preload: false });
 const hepta_slab = Hepta_Slab({ preload: false });
 const honk = Honk({ preload: false });
-
+const ibarra_real_nova = Ibarra_Real_Nova({ preload: false });
+const imbue = Imbue({ preload: false });
+const inconsolata = Inconsolata({ preload: false });
+const instrument_sans = Instrument_Sans({ preload: false });
+const inter = Inter({ preload: false });
+const inter_tight = Inter_Tight({ preload: false });
+const jetbrains_mono = JetBrains_Mono({ preload: false });
+const josefin_sans = Josefin_Sans({ preload: false });
+const josefin_slab = Josefin_Slab({ preload: false });
+const jost = Jost({ preload: false });
+const jura = Jura({ preload: false });
+const kablammo = Kablammo({ preload: false });
+const kalnia = Kalnia({ preload: false });
+const kameron = Kameron({ preload: false });
+const karla = Karla({ preload: false });
+const kode_mono = Kode_Mono({ preload: false });
+const kreon = Kreon({ preload: false });
+const kufam = Kufam({ preload: false });
+const kumbh_sans = Kumbh_Sans({ preload: false });
+const labrada = Labrada({ preload: false });
+const league_gothic = League_Gothic({ preload: false });
+const lemonada = Lemonada({ preload: false });
+const lexend = Lexend({ preload: false });
+const lexend_deca = Lexend_Deca({ preload: false });
+const lexend_giga = Lexend_Giga({ preload: false });
+const lexend_mega = Lexend_Mega({ preload: false });
+const lexend_peta = Lexend_Peta({ preload: false });
+const lexend_tera = Lexend_Tera({ preload: false });
+const lexend_zetta = Lexend_Zetta({ preload: false });
+const libre_bodoni = Libre_Bodoni({ preload: false });
+const libre_franklin = Libre_Franklin({ preload: false });
+const linefont = Linefont({ preload: false });
+const literata = Literata({ preload: false });
+const lora = Lora({ preload: false });
+const m_plus_1 = M_PLUS_1({ preload: false });
+const m_plus_1_code = M_PLUS_1_Code({ preload: false });
+const m_plus_2 = M_PLUS_2({ preload: false });
+const m_plus_code_latin = M_PLUS_Code_Latin({ preload: false });
+const mada = Mada({ preload: false });
+const manrope = Manrope({ preload: false });
+const marhey = Marhey({ preload: false });
+const markazi_text = Markazi_Text({ preload: false });
+const martian_mono = Martian_Mono({ preload: false });
+const maven_pro = Maven_Pro({ preload: false });
+const meera_inimai = Meera_Inimai({ preload: false,weight:"400" });
+const merriweather_sans = Merriweather_Sans({ preload: false });
+const mohave = Mohave({ preload: false });
+const montagu_slab = Montagu_Slab({ preload: false });
+const montserrat = Montserrat({ preload: false });
+const mulish = Mulish({ preload: false });
+const museo_moderno = MuseoModerno({ preload: false });
+const nabla = Nabla({ preload: false });
+const noto_emoji = Noto_Emoji({ preload: false });
+const noto_kufi_arabic = Noto_Kufi_Arabic({ preload: false });
+const noto_naskh_arabic = Noto_Naskh_Arabic({ preload: false });
+const noto_nastaliq_urdu = Noto_Nastaliq_Urdu({ preload: false });
+const noto_rashi_hebrew = Noto_Rashi_Hebrew({ preload: false });
+const noto_sans = Noto_Sans({ preload: false });
+const noto_sans_adlam = Noto_Sans_Adlam({ preload: false });
+const noto_sans_adlam_unjoined = Noto_Sans_Adlam_Unjoined({ preload: false });
+const noto_sans_arabic = Noto_Sans_Arabic({ preload: false });
+const nunito = Nunito({ preload: false });
+const nunito_sans = Nunito_Sans({ preload: false });
+const ojuju = Ojuju({ preload: false });
+const onest = Onest({ preload: false });
+const open_sans = Open_Sans({ preload: false });
+const orbitron = Orbitron({ preload: false });
+const oswald = Oswald({ preload: false });
+const outfit = Outfit({ preload: false });
+const overpass = Overpass({ preload: false });
+const overpass_mono = Overpass_Mono({ preload: false });
+const oxanium = Oxanium({ preload: false });
+const pathway_extreme = Pathway_Extreme({ preload: false });
+const petrona = Petrona({ preload: false });
+const phudu = Phudu({ preload: false });
+const piazzolla = Piazzolla({ preload: false });
+const pixelify_sans = Pixelify_Sans({ preload: false });
+const platypi = Platypi({ preload: false });
+const playfair = Playfair({ preload: false });
+const playfair_display = Playfair_Display({ preload: false });
+const playpen_sans = Playpen_Sans({ preload: false });
+const plus_jakarta_sans = Plus_Jakarta_Sans({ preload: false });
+const podkova = Podkova({ preload: false });
+const poltawski_nowy = Poltawski_Nowy({ preload: false });
+const pontano_sans = Pontano_Sans({ preload: false });
+const public_sans = Public_Sans({ preload: false });
+const quicksand = Quicksand({preload:false});
+const rem = REM({ preload: false });
+const radio_canada = Radio_Canada({ preload: false });
+const raleway = Raleway({ preload: false });
+const rasa = Rasa({ preload: false });
+const readex_pro = Readex_Pro({ preload: false });
+const recursive = Recursive({ preload: false });
+const red_hat_display = Red_Hat_Display({ preload: false });
+const red_hat_mono = Red_Hat_Mono({ preload: false });
+const red_hat_text = Red_Hat_Text({ preload: false });
+const red_rose = Red_Rose({ preload: false });
+const reddit_mono = Reddit_Mono({ preload: false });
+const reem_kufi = Reem_Kufi({ preload: false });
+const reem_kufi_fun = Reem_Kufi_Fun({ preload: false });
+const rethink_sans = Rethink_Sans({ preload: false });
+const roboto_condensed = Roboto_Condensed({ preload: false });
+const roboto_flex = Roboto_Flex({ preload: false });
+const roboto_mono = Roboto_Mono({ preload: false });
+const roboto_serif = Roboto_Serif({ preload: false });
+const roboto_slab = Roboto_Slab({ preload: false });
+const rosario = Rosario({ preload: false });
+const rubik = Rubik({ preload: false });
+const ruda = Ruda({ preload: false });
+const stix_two_text = STIX_Two_Text({ preload: false });
+const saira = Saira({ preload: false });
+const sansita_swashed = Sansita_Swashed({ preload: false });
+const schibsted_grotesk = Schibsted_Grotesk({ preload: false });
+const sen = Sen({ preload: false });
+const shantell_sans = Shantell_Sans({ preload: false });
+const signika = Signika({ preload: false });
+const sixtyfour = Sixtyfour({ preload: false });
+const smooch_sans = Smooch_Sans({ preload: false });
+const sofia_sans = Sofia_Sans({ preload: false });
+const sofia_sans_condensed = Sofia_Sans_Condensed({ preload: false });
+const sofia_sans_extra_condensed = Sofia_Sans_Extra_Condensed({ preload: false });
+const sofia_sans_semi_condensed = Sofia_Sans_Semi_Condensed({ preload: false });
+const sometype_mono = Sometype_Mono({ preload: false });
+const sono = Sono({ preload: false });
+const sora = Sora({ preload: false });
+const source_code_pro = Source_Code_Pro({ preload: false });
+const source_sans_3 = Source_Sans_3({ preload: false });
+const source_serif_4 = Source_Serif_4({ preload: false });
+const space_grotesk = Space_Grotesk({ preload: false });
+const spline_sans = Spline_Sans({ preload: false });
+const spline_sans_mono = Spline_Sans_Mono({ preload: false });
+const syne = Syne({ preload: false });
+const teko = Teko({ preload: false });
+const tektur = Tektur({ preload: false });
+const texturina = Texturina({ preload: false });
+const tilt_neon = Tilt_Neon({ preload: false });
+const tilt_prism = Tilt_Prism({ preload: false });
+const tourney = Tourney({ preload: false });
+const trispace = Trispace({ preload: false });
+const truculenta = Truculenta({ preload: false });
+const ubuntu = Ubuntu({ preload: false,weight:"400" });
+const ubuntu_mono = Ubuntu_Mono({ preload: false,weight:"400" });
+const unbounded = Unbounded({ preload: false });
+const urbanist = Urbanist({ preload: false });
+const varta = Varta({ preload: false });
+const vazirmatn = Vazirmatn({ preload: false });
+const victor_mono = Victor_Mono({ preload: false });
+const vollkorn = Vollkorn({ preload: false });
+const wavefont = Wavefont({ preload: false });
+const wix_madefor_display = Wix_Madefor_Display({ preload: false });
+const wix_madefor_text = Wix_Madefor_Text({ preload: false });
+const work_sans = Work_Sans({ preload: false });
+const workbench = Workbench({ preload: false });
+const yaldevi = Yaldevi({ preload: false });
+const yanone_kaffeesatz = Yanone_Kaffeesatz({ preload: false });
+const yrsa = Yrsa({ preload: false });
+const ysabeau = Ysabeau({ preload: false });
+const ysabeau_infant = Ysabeau_Infant({ preload: false });
+const ysabeau_office = Ysabeau_Office({ preload: false });
+const ysabeau_sc = Ysabeau_SC({ preload: false });
 
 export {
   ar_one_sans,
@@ -334,5 +655,165 @@ export {
   hedvig_letters_serif,
   heebo,
   hepta_slab,
-  honk
+  honk,
+  ibarra_real_nova,
+  imbue,
+  inconsolata,
+  instrument_sans,
+  inter,
+  inter_tight,
+  jetbrains_mono,
+  josefin_sans,
+  josefin_slab,
+  jost,
+  jura,
+  kablammo,
+  kalnia,
+  kameron,
+  karla,
+  kode_mono,
+  kreon,
+  kufam,
+  kumbh_sans,
+  labrada,
+  league_gothic,
+  lemonada,
+  lexend,
+  lexend_deca,
+  lexend_giga,
+  lexend_mega,
+  lexend_peta,
+  lexend_tera,
+  lexend_zetta,
+  libre_bodoni,
+  libre_franklin,
+  linefont,
+  literata,
+  lora,
+  m_plus_1,
+  m_plus_1_code,
+  m_plus_2,
+  m_plus_code_latin,
+  mada,
+  manrope,
+  marhey,
+  markazi_text,
+  martian_mono,
+  maven_pro,
+  meera_inimai,
+  merriweather_sans,
+  mohave,
+  montagu_slab,
+  montserrat,
+  mulish,
+  museo_moderno,
+  nabla,
+  noto_emoji,
+  noto_kufi_arabic,
+  noto_naskh_arabic,
+  noto_nastaliq_urdu,
+  noto_rashi_hebrew,
+  noto_sans,
+  noto_sans_adlam,
+  noto_sans_adlam_unjoined,
+  noto_sans_arabic,
+  nunito,
+  nunito_sans,
+  ojuju,
+  onest,
+  open_sans,
+  orbitron,
+  oswald,
+  outfit,
+  overpass,
+  overpass_mono,
+  oxanium,
+  pathway_extreme,
+  petrona,
+  phudu,
+  piazzolla,
+  pixelify_sans,
+  platypi,
+  playfair,
+  playfair_display,
+  playpen_sans,
+  plus_jakarta_sans,
+  podkova,
+  poltawski_nowy,
+  pontano_sans,
+  public_sans,
+  quicksand,
+  rem,
+  radio_canada,
+  raleway,
+  rasa,
+  readex_pro,
+  recursive,
+  red_hat_display,
+  red_hat_mono,
+  red_hat_text,
+  red_rose,
+  reddit_mono,
+  reem_kufi,
+  reem_kufi_fun,
+  rethink_sans,
+  roboto_condensed,
+  roboto_flex,
+  roboto_mono,
+  roboto_serif,
+  roboto_slab,
+  rosario,
+  rubik,
+  ruda,
+  stix_two_text,
+  saira,
+  sansita_swashed,
+  schibsted_grotesk,
+  sen,
+  shantell_sans,
+  signika,
+  sixtyfour,
+  smooch_sans,
+  sofia_sans,
+  sofia_sans_condensed,
+  sofia_sans_extra_condensed,
+  sofia_sans_semi_condensed,
+  sometype_mono,
+  sono,
+  sora,
+  source_code_pro,
+  source_sans_3,
+  source_serif_4,
+  space_grotesk,
+  spline_sans,
+  spline_sans_mono,
+  syne,
+  teko,
+  tektur,
+  texturina,
+  tilt_neon,
+  tilt_prism,
+  tourney,
+  trispace,
+  truculenta,
+  ubuntu,
+  ubuntu_mono,
+  unbounded,
+  urbanist,
+  varta,
+  vazirmatn,
+  victor_mono,
+  vollkorn,
+  wavefont,
+  wix_madefor_display,
+  wix_madefor_text,
+  work_sans,
+  workbench,
+  yaldevi,
+  yanone_kaffeesatz,
+  yrsa,
+  ysabeau,
+  ysabeau_infant,
+  ysabeau_office,
+  ysabeau_sc
 };
