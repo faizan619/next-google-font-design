851eb801bc5e9c3b329820bb4e4b618010e692be
faizan619 <alamf6023@gmail.com>
2024-05-13 14:05:09 +0530

    added copy btn

--- src/app/fontstyle/Home.jsx
+++ src/app/fontstyle/Home.jsx
@@ -1,17 +1,28 @@
-"use client"
+"use client";
 import { fontName } from "@/app/style/font_variable";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
 
-  const handleView = (name)=>{
+  const handleView = (name) => {
     router.push(`/fontstyle/demo?font=${name}`);
-  }
+  };
 
-  const handleCopy = ()=>{
-    alert("copied!")
-  }
+  const handleCopy = (key) => {
+    try {
+      navigator.clipboard
+        .writeText(key)
+        .then(() => {
+          alert(`Copied: ${key}`);
+        })
+        .catch((err) => {
+          alert("Failed to Copy!");
+        });
+    } catch (err) {
+      alert("Failed to Copy!");
+    }
+  };
   return (
     <>
       <div className="min-h-screen flex justify-center items-center flex-col gap-5 p-5 pt-24">
@@ -20,25 +31,29 @@ export default function Home() {
           <p>Weight:400</p>
           <p>Copy The Font</p>
         </div>
-        {Object.keys(fontName).map((key, index) => (
-          <div className={`w-full flex justify-center items-center flex-col`} >
+        <div className={`w-full flex justify-center gap-5 transition-all items-center flex-col`}>
+          {Object.keys(fontName).map((key, index) => (
             <div
               key={key}
-              className={`${fontName[key].className}  cursor-pointer  text-2xl border p-4 rounded-md shadow-sm shadow-gray-600 md:hover:scale-105 hover:underline transition-all flex justify-between items-center md:w-[70%] w-[100%]`}
-              onClick={()=>{handleView(`${key}`)}}
+              className={`${fontName[key].className}  cursor-pointer  text-2xl border p-4 rounded-md shadow-sm shadow-gray-600 md:hover:scale-105 transition-all flex justify-between items-center md:w-[70%] w-[100%]`}
             >
-              <div className="flex gap-2">
+              <div
+                className="flex gap-2 hover:underline"
+                onClick={() => {
+                  handleView(`${key}`);
+                }}
+              >
                 <p>{key}</p>
               </div>
               <button
-              onClick={handleCopy}
+                onClick={() => handleCopy(key)}
                 className={`border border-gray-600 px-2 rounded-md bg-black text-white  text-center font-serif hover:scale-105`}
               >
                 Copy
               </button>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </>
   );
