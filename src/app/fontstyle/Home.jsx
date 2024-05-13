1a9330d606095e88f916fe698f5653549d954e6b
faizan619 <alamf6023@gmail.com>
2024-05-13 12:28:21 +0530

    created a demo page to view all the text style

--- src/app/fontstyle/Home.jsx
+++ src/app/fontstyle/Home.jsx
@@ -1,25 +1,42 @@
+"use client"
 import { fontName } from "@/app/style/font_variable";
+import { useRouter } from "next/navigation";
+import Demo from "./demo/page";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleView = (name)=>{
+    router.push(`/fontstyle/demo?font=${name}`);
+  }
   return (
     <>
       <div className="min-h-screen flex justify-center items-center flex-col gap-5 p-5 pt-24">
-          <div className="border w-[93%] md:w-[78%] bg-gray-100 p-5 rounded-md shadow-sm shadow-gray-600 fixed top-1 z-50 flex justify-between">
-            <p>Font Name</p>
-            <p>Weight:400</p>
-            <p>Copy The Font</p>
-          </div>
-          {Object.keys(fontName).map((key,index) => (
+        <div className="border w-[93%] md:w-[78%] bg-gray-100 p-5 rounded-md shadow-sm shadow-gray-600 fixed top-1 z-50 flex justify-between">
+          <p>Font Name</p>
+          <p>Weight:400</p>
+          <p>Copy The Font</p>
+        </div>
+        {Object.keys(fontName).map((key, index) => (
+          <div className={`w-full flex justify-center items-center flex-col`}>
             <div
               key={key}
-              className={`${fontName[key].className} text-2xl border p-5 rounded-md shadow-sm shadow-gray-600 hover:scale-105 transition-all flex justify-between md:w-[70%] w-[100%]`}>
+              className={`${fontName[key].className} cursor-pointer  text-2xl border p-4 rounded-md shadow-sm shadow-gray-600 md:hover:scale-105 hover:underline transition-all flex justify-between items-center md:w-[70%] w-[100%]`}
+            >
               <div className="flex gap-2">
-                <p>[ {index} ]</p>
-              <p>{key}</p>
+                <p>{key}</p>
               </div>
-              <button className={`border border-gray-600 px-2 rounded-md bg-black text-white  text-center font-serif`}>Copy</button>
+              <div className="flex gap-2">
+              <button
+                className={`border border-gray-600 px-2 rounded-md bg-black text-white  text-center font-serif hover:scale-105`}
+              >
+                Copy
+              </button>
+              <button className="font-serif" onClick={()=>{handleView(`${key}`)}}>View</button>
+              </div>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
     </>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Main() {
   return (
     <div className="flex flex-col items-center h-screen pt-8 bg-gray-200">
       <Link href="/fontstyle" className="border rounded-md p-3 fontName show h-28 w-64 cursor-pointer hover:scale-105 transition-all shadow-md hover:shadow-black">
-        <p className="fixed top-28 show1 hidden w-56 text-center shadow-md shadow-black rounded-md bg-white px-5 mt-3">View All Nextjs Google Fonts</p>
+        <p className="fixed top-28 show1 hidden w-56 text-center shadow-md shadow-black rounded-md bg-white px-5 mt-3">View All Nextjs Google Fonts <br/> Total: 270 Fonts</p>
       </Link>
     </div>
   );
