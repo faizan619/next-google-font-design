9989f58a8cbb98a6f94cc350a4a20792dc84998c
faizan619 <alamf6023@gmail.com>
2024-05-15 09:55:32 +0530

    change logo

--- src/app/saved/SaveFont.js
+++ src/app/saved/SaveFont.js
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
@@ -53,6 +54,10 @@ const Saves = () => {
     <div className="p-3">
       <h2>Total Fonts Saved : {fontItems?.length}</h2>
       <div className="flex flex-col items-center gap-5 mt-4">
+      {fontItems?.length === 0?(<div className="flex flex-col gap-5 items-center justify-center">
+        <p>Go to Font Page to View amazing Nextjs fonts</p>
+        <Link href="/fontstyle" className="border px-5 py-2 shadow-sm hover:shadow-md hover:shadow-black transition-all shadow-black rounded-md">View Font</Link>
+      </div>):(null)}
         {fontItems.map((item) => {
           return (
             <div
